b37bfe1048d95fac8093d648e5930e036113fd66
AldoOps <aldo.santos.oliveira95@gmail.com>
2024-08-17 15:29:28 -0300

    update output

--- modules/vpc/netwok.tf
+++ modules/vpc/netwok.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpctf" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -7,5 +7,5 @@ resource "aws_vpc" "main" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.vpctf.id
 }
